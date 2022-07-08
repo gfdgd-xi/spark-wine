beaa19a6b3436ea4ae441de64270f5015e0af4ba
gfdgd xi <73065138+gfdgd-xi@users.noreply.github.com>
2022-07-08 22:59:50 +0800

    优化了星火应用商店源的获取

--- Debain发行版安装spark-wine7-devel.py
+++ Debain发行版安装spark-wine7-devel.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # 使用系统默认的 python3 运行
 ###########################################################################################
-# 作者：gfdgd xi
-# 版本：1.4.1
-# 更新时间：2022年06月22日
-# 感谢：感谢 wine 以及 deepin-wine 团队，提供了 wine 和 deepin-wine 给大家使用，让gfdgd xi能做这个程序
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 版本：1.5.3
+# 更新时间：2022年07月08日
+# 感谢：感谢 wine 以及 deepin-wine 团队，提供了 wine 和 deepin-wine 给大家使用，让我能做这个程序
 # 基于 Python3 的 tkinter 构建
 ###########################################################################################
 #################
@@ -23,28 +23,43 @@ print("请问是否要更新操作系统？[Y/N]", end=' ')
 choose = input().upper()
 if not choose == "N":
     os.system("sudo apt upgrade -y")
-print("请你务必安装 apt-fast（需要添加星火应用商店的源）？[Y/N]", end=' ')
+print("请问是否要安装原版 wine（wine64）？[Y/N]", end=' ')
 choose = input().upper()
 if not choose == "N":
+    os.system("sudo apt install wine -y")
+print("请问是否要安装 deepin-wine？[Y/N]", end=' ')
+choose = input().upper()
+if not choose == "N":
+    os.system("sudo apt install deepin-wine -y")
+print("请问是否要安装 deepin-wine5（需要添加星火应用商店的源）？[Y/N]", end=' ')
+choose = input().upper() 
+if not choose == "N":  # 获取方式来自于星火应用商店 deb 安装包
     if os.path.exists("/etc/apt/sources.list.d/sparkstore.list"):
-        os.system("sudo apt install apt-fast -y")
+        os.system("sudo apt install deepin-wine5 -y")
     if not os.path.exists("/etc/apt/sources.list.d/sparkstore.list"):
         os.system("sudo touch /etc/apt/sources.list.d/sparkstore.list")
         os.system("echo 'deb [by-hash=force] https://d.store.deepinos.org.cn/ /' | sudo tee '/etc/apt/sources.list.d/sparkstore.list'")
+
         os.system("sudo apt update")
-    os.system("sudo apt install apt-fast -y")
-    if os.path.exists("/etc/apt/sources.list.d/sparkstore.list"):
-        os.system("sudo apt update")
-print("请问是否要安装 spark-wine7-devel（需要添加星火应用商店的源）？[Y/N]", end=' ')
+        
+        os.system("sudo apt install deepin-wine5 -y")
+print("请问是否要安装 deepin-wine5-stable？[Y/N]", end=' ')
 choose = input().upper()
 if not choose == "N":
+    os.system("sudo apt install deepin-wine5-stable -y")
+print("请问是否要安装 deepin-wine6-stable？[Y/N]", end=' ')
+choose = input().upper()
+if not choose == "N":
+    os.system("sudo apt install deepin-wine6-stable -y")
+print("请问是否要安装 spark-wine7-devel？[Y/N]", end=' ')
+choose = input().upper()
+if not choose == "N":  # 获取方式来自于星火应用商店 deb 安装包
     if os.path.exists("/etc/apt/sources.list.d/sparkstore.list"):
-        os.system("sudo ss-apt-fast install spark-wine7-devel -y")
+        os.system("sudo apt install spark-wine7-devel -y")
     if not os.path.exists("/etc/apt/sources.list.d/sparkstore.list"):
         os.system("sudo touch /etc/apt/sources.list.d/sparkstore.list")
         os.system("echo 'deb [by-hash=force] https://d.store.deepinos.org.cn/ /' | sudo tee '/etc/apt/sources.list.d/sparkstore.list'")
+
         os.system("sudo apt update")
-    os.system("sudo ss-apt-fast install spark-wine7-devel -y")
-    if os.path.exists("/etc/apt/sources.list.d/sparkstore.list"):
-        os.system("sudo apt update")
+        os.system("sudo apt install spark-wine7-devel -y")
 print("全部完成！")
